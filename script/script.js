94c54aa9db779c2bd4074cc5a07b7e8dc510aef0
LucasOliveiraaq <lucasp.oliveira122@gmail.com>
2024-07-31 21:35:30 -0300

    feat: script

---
+++ script/script.js
@@ -0,0 +1,12 @@
+//home.html
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
